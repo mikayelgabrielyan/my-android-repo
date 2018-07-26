5d10001af50b4b11e9ad4cb528d9f1e95418a7f6
Mikayel Gabrielyan <mikayel93gabrielyan@gmail.com>
2018-07-27 01:38:38 +0400

    Done Login UI and errors

--- kitchen/LoginPage/app/src/main/java/project/kitchen/itc9/loginpage/LoginActivity.java
+++ kitchen/LoginPage/app/src/main/java/project/kitchen/itc9/loginpage/LoginActivity.java
@@ -1,13 +1,115 @@
 package project.kitchen.itc9.loginpage;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+    private static final String TAG = "LoginActivity";
+
+    EditText mEmailText;
+    EditText mPasswordText;
+    Button mLoginButton;
+    TextView mForgotButton;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mEmailText = findViewById(R.id.input_email);
+        mPasswordText = findViewById(R.id.input_password);
+        mLoginButton = findViewById(R.id.btn_login);
+        mForgotButton = findViewById(R.id.forgot_pass_btn);
+
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+
+        mForgotButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, ForgotPasswordActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void login() {
+        Log.d(TAG, "Login");
+
+        if (!validate()) {
+            onLoginFailed();
+            return;
+        }
+
+        mLoginButton.setEnabled(false);
+
+        final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this, ProgressDialog.THEME_HOLO_DARK);
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setTitle("Please wait");
+        progressDialog.setMessage("Authenticating...");
+        progressDialog.show();
+
+        new android.os.Handler().postDelayed(
+            new Runnable() {
+                public void run() {
+                    onLoginSuccess();
+                    progressDialog.dismiss();
+                }
+            }, 5000);
+        Intent intent = new Intent(LoginActivity.this, HomePageActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onBackPressed() {
+        moveTaskToBack(true);
+    }
+
+    public void onLoginSuccess() {
+        mLoginButton.setEnabled(true);
+        finish();
+    }
+
+    public void onLoginFailed() {
+        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
+        mLoginButton.setEnabled(true);
+    }
+
+    public boolean validate() {
+        boolean valid = true;
+
+        String email = mEmailText.getText().toString();
+        String password = mPasswordText.getText().toString();
+
+        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            mEmailText.setError("enter a valid email address");
+            valid = false;
+        } else if(!email.equals("user@gmail.com") && !password.equals("useruser")) {
+            mEmailText.setError("wrong user log");
+            valid = false;
+        } else {
+            mEmailText.setError(null);
+        }
+
+        if (password.isEmpty() || password.length() < 6 || password.length() > 11) {
+            mPasswordText.setError("between 6 and 10 alphanumeric characters");
+            valid = false;
+        } else {
+            mPasswordText.setError(null);
+        }
+        return valid;
     }
 }
