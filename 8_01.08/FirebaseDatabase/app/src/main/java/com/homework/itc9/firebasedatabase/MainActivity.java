b4a11d94d90dedd59ea47fb81e8d9e2d52ef5c40
Mikayel Gabrielyan <mikayel93gabrielyan@gmail.com>
2018-08-02 11:04:07 +0400

    Done FirebaseDatabase

--- 8_01.08/FirebaseDatabase/app/src/main/java/com/homework/itc9/firebasedatabase/MainActivity.java
+++ 8_01.08/FirebaseDatabase/app/src/main/java/com/homework/itc9/firebasedatabase/MainActivity.java
@@ -1,7 +1,7 @@
 package com.homework.itc9.firebasedatabase;
 
-import android.app.ListActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatButton;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -14,12 +14,12 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 
-public class MainActivity extends ListActivity {
-    EditText editText;
-    AppCompatButton saveBtn;
-    ListView listView;
-    ArrayList<String> listItems;
-    ArrayAdapter<String> adapter;
+public class MainActivity extends AppCompatActivity {
+    private EditText editText;
+    private AppCompatButton saveBtn;
+    private ListView listView;
+    private ArrayList<String> listItems;
+    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,29 +31,29 @@ public class MainActivity extends ListActivity {
         listView = findViewById(R.id.sampleListView);
         saveBtn = findViewById(R.id.save_btn);
         listItems = new ArrayList<>();
-        adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,listItems);
-        setListAdapter(adapter);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listItems);
+        listView.setAdapter(adapter);
         saveBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String text = editText.getText().toString();
-                if (!text.matches("")) {
+                final String text = editText.getText().toString();
+                if (!text.isEmpty()) {
                     myRef.setValue(text);
                 }
-                myRef.addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        String data = dataSnapshot.getValue(String.class);
-                        editText.setText(null);
-                        listItems.add(data);
-                        adapter.notifyDataSetChanged();
-                    }
-                    @Override
-                    public void onCancelled(DatabaseError error) {
-                    }
-
-                });
             }
+        });
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String data = dataSnapshot.getValue(String.class);
+                editText.setText(null);
+                listItems.add(data);
+                adapter.notifyDataSetChanged();
+            }
+            @Override
+            public void onCancelled(DatabaseError error) {
+            }
+
         });
     }
 }
